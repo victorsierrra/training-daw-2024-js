5fae246a3ec8859777d5b31947af2901af36f9b8
Victor <a27300@valero.com>
2024-11-05 20:54:04 +0100

    Finalizacion Unidad1

--- UD1/07_insert_if_not_exists/main.js
+++ UD1/07_insert_if_not_exists/main.js
@@ -1,6 +1,18 @@
 const assert = require('assert').strict
 
 function insertIfNotExists(array, item, position) {
+    if (!array.includes(item))
+    {
+        if (position == false){
+            array.push(item)
+        }
+        else {
+            array.unshift(item)
+        }
+    }
+    else{
+        console.log("Nada")
+    }
     return array
 }
 
@@ -10,9 +22,9 @@ let array = ['pera', 'manzana']
 let result = insertIfNotExists(array, 'pera', false)
 assert.deepStrictEqual(result, ['pera', 'manzana'])
 
-// result = insertIfNotExists(array, 'melón', false)
-// assert.deepStrictEqual(result, ['pera', 'manzana', 'melón'])
+result = insertIfNotExists(array, 'melón', false)
+assert.deepStrictEqual(result, ['pera', 'manzana', 'melón'])
 
-// result = insertIfNotExists(array, 'melocotón', true)
-// assert.deepStrictEqual(result, ['melocotón', 'pera', 'manzana', 'melón'])
+result = insertIfNotExists(array, 'melocotón', true)
+assert.deepStrictEqual(result, ['melocotón', 'pera', 'manzana', 'melón'])
 
--- UD1/10_seconds_converter/main.js
+++ UD1/10_seconds_converter/main.js
@@ -1,10 +1,16 @@
 const assert = require('assert').strict;
 
 function toHoursMinutesSeconds(value) {
-    let minutos = value / 60
-    let horas = minutos / 60
+    let horas = value / 3600
+    horas = Math.round(horas)
+    let minutos = value % 3600 / 60
+    minutos = Math.round(minutos)
+    let segundos = value %60
+    segundos = Math.round(segundos)
+    return `${horas}:${minutos}:${segundos}`
+
 }
 
 assert.deepStrictEqual(toHoursMinutesSeconds(3600), "1:0:0")
-assert.deepStrictEqual(toHoursMinutesSeconds(3720), "1:20:0")
-assert.deepStrictEqual(toHoursMinutesSeconds(3725), "1:20:5")
+assert.deepStrictEqual(toHoursMinutesSeconds(3720), "1:2:0")
+assert.deepStrictEqual(toHoursMinutesSeconds(3725), "1:2:5")
